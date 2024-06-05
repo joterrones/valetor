85c2a6b34bc32d6b4f31d83a4c785ccda2966101
Jonathan Terrones <joterronestr@hotmail.com>
2024-06-05 02:04:20 -0500

    Primeras pantallas

--- backend/valetor_backend/dal/movil.js
+++ backend/valetor_backend/dal/movil.js
@@ -37,6 +37,158 @@ const getusuario = async (request, response) => {
 
 }
 
+const insertarventa = async (request, response) => {
+    
+    try {
+        let scriptSQL = "insert into gen_venta (n_idseg_vendedor, n_idgen_equipo, n_borrado, id_usercrea, d_fechacrea)values("+request.body.n_idseg_vendedor+","+request.body.n_idgen_equipo+",0,"+request.body.id_usercrea+",now()) returning *";
+        console.log(scriptSQL)
+        let queryUsuario = await pool.query(scriptSQL);
+        if(queryUsuario.rowCount>0){
+            response.status(200).json({
+                    data:queryUsuario.rows,
+                    flag: true,
+                    mensaje: "Venta registrada correcta"
+                });
+        }else{
+            response.status(200).json({
+                data:null,
+                    flag: false,
+                    mensaje: "No se registró la venta"
+                });
+        }
+     
+    } catch (error) {
+        response.status(200).json({
+            data:null,
+                flag: false,
+                mensaje: "Error al registrar la venta"
+            });
+    }
+
+}
+
+const actualizarhoraventa = async (request, response) => {
+    
+    try {
+        let scriptSQL = "update gen_venta set n_hora ="+request.body.n_hora+", id_usermodi = "+request.body.n_idseg_vendedor+",d_fechamodi = now()) returning *";
+        console.log(scriptSQL)
+        let queryUsuario = await pool.query(scriptSQL);
+        if(queryUsuario.rowCount>0){
+            response.status(200).json({
+                    data:queryUsuario.rows,
+                    flag: true,
+                    mensaje: "Venta registrada correcta"
+                });
+        }else{
+            response.status(200).json({
+                data:null,
+                    flag: false,
+                    mensaje: "No se registró la venta"
+                });
+        }
+     
+    } catch (error) {
+        response.status(200).json({
+            data:null,
+                flag: false,
+                mensaje: "Error al registrar la venta"
+            });
+    }
+
+}
+
+const actualizarfechaventa = async (request, response) => {
+    
+    try {
+        let scriptSQL = "update gen_venta set d_fecha ="+request.body.d_fecha+", id_usermodi = "+request.body.n_idseg_vendedor+",d_fechamodi = now()) returning *";
+        console.log(scriptSQL)
+        let queryUsuario = await pool.query(scriptSQL);
+        if(queryUsuario.rowCount>0){
+            response.status(200).json({
+                    data:queryUsuario.rows,
+                    flag: true,
+                    mensaje: "Fecha inicio actualzada correctamente"
+                });
+        }else{
+            response.status(200).json({
+                data:null,
+                    flag: false,
+                    mensaje: "No se actualizó la fecha"
+                });
+        }
+     
+    } catch (error) {
+        response.status(200).json({
+            data:null,
+                flag: false,
+                mensaje: "Error al actualizar la fecha"
+            });
+    }
+
+}
+
+const insertardetalleventa = async (request, response) => {
+    
+    try {
+        let scriptSQL = "insert into gen_detalle_venta (n_idgen_venta, n_idgen_producto, n_cantidad, n_subtotal, b_pagado,  n_borrado, id_usercrea, d_fechacrea)values("+request.body.n_idgen_venta+","+request.body.n_idgen_producto+","+request.body.n_cantidad+","+request.body.n_subtotal+","+request.body.b_pagado+",0,"+request.body.id_usercrea+",now()) returning *";
+        console.log(scriptSQL)
+        let queryUsuario = await pool.query(scriptSQL);
+        if(queryUsuario.rowCount>0){
+            response.status(200).json({
+                    data:queryUsuario.rows,
+                    flag: true,
+                    mensaje: "Detalle de venta registrada correctamente"
+                });
+        }else{
+            response.status(200).json({
+                data:null,
+                    flag: false,
+                    mensaje: "No se registró el detalle de la venta"
+                });
+        }
+     
+    } catch (error) {
+        response.status(200).json({
+            data:null,
+                flag: false,
+                mensaje: "Error al registrar el detalle de la venta"
+            });
+    }
+
+}
+
+const actualizarpagodetalleventa = async (request, response) => {
+    
+    try {
+        let scriptSQL = "update gen_detalle_venta set b_pagado ="+request.body.b_pagado+", id_usermodi = "+request.body.n_idseg_vendedor+",d_fechamodi = now()) returning *";
+        console.log(scriptSQL)
+        let queryUsuario = await pool.query(scriptSQL);
+        if(queryUsuario.rowCount>0){
+            response.status(200).json({
+                    data:queryUsuario.rows,
+                    flag: true,
+                    mensaje: "El pago se actualizó correctamente"
+                });
+        }else{
+            response.status(200).json({
+                data:null,
+                    flag: false,
+                    mensaje: "No se actualizó el pago"
+                });
+        }
+     
+    } catch (error) {
+        response.status(200).json({
+            data:null,
+                flag: false,
+                mensaje: "Error al actualizar pago"
+            });
+    }
+
+}
+
+
+
 const getdato = async (request, response) => {
     console.log("getdatos");
 
@@ -59,6 +211,8 @@ const getdato = async (request, response) => {
 
 module.exports = {
     getusuario,
-    getdato
+    getdato,
+    insertarventa,
+    insertardetalleventa,
 }
 
--- backend/valetor_backend/index.js
+++ backend/valetor_backend/index.js
@@ -37,6 +37,8 @@ app.use('/icons', express.static(__dirname + icon));
 /* Movil */
 app.post('/api/movil/getusuario', dbMovil.getusuario,);
 app.get('/api/movil/getdato', dbMovil.getdato);
+app.post('/api/movil/insertarventa', dbMovil.insertarventa,);
+app.post('/api/movil/insertardetalleventa', dbMovil.insertardetalleventa,);
 
 server.listen(port, function () {
   console.log('\n')
